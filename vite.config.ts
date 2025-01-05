2208c1286408d588a10394f822a3558306bd54d6
Jiri Peterek <jiri@peterek.net>
2025-01-04 20:21:02 -0600

    build!: update dependencies
    BREAKING: drop support for Node.js versions earlier than 20

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
 import { resolve } from 'node:path'
 import { defineConfig } from 'vite'
 import dts from 'vite-plugin-dts'
-import pkg from './package.json' assert { type: 'json' }
+import pkg from './package.json'
 
 export default defineConfig({
   build: {
